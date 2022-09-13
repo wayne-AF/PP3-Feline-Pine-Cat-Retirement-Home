85c6b9287bf2aad23ea3242a6bb4c91cdbd8fbe1
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-09-13 14:31:26 +0000

    update add_new_resident and update_worksheets_new_resident to add resident weight data to weight worksheet

--- run.py
+++ run.py
@@ -23,12 +23,6 @@ residents_ideal_weight = SHEET.worksheet("current residents").col_values(5)
 recent_weight_data = SHEET.worksheet("weight").row_values
 
 
-# def residents():
-#     print(residents_list)
-#     print(residents_ideal_weight)
-
-# residents()
-
 def clear():
     """
     Clears screen
@@ -45,17 +39,17 @@ def get_weight(residents):
     clear()
     weight_data = []
     print(" * * Weight Log Section * *\n")
-    print(" Update residents' weight here.\n")
-    print(" The entry should be in kilograms.\n")
+    print("Update residents' weight here.\n")
+    print("The entry should be in kilograms.\n")
     for i in residents:
         while True:
             try:
-                weight = float(input(f" Enter {i}'s weight:\n"))
+                weight = float(input(f"Enter {i}'s weight:\n"))
                 break
             except ValueError:
-                print(" That is not a valid entry! Please enter a valid weight.\n")
+                print("That is not a valid entry! Please enter a valid weight.\n")
 
-        weight_data.append(weight)
+        weight_data.append(weight)    
 
     return weight_data
 
@@ -69,12 +63,40 @@ def update_weight_worksheet(weight):
     weight_worksheet.append_row(weight)
     print("Purrfect! Thank you for updating everyone's weight!\n")
 
+    while True:
+        selection = input("Use any key to return to the previous menu.\n")
+        if selection:
+            weight_log_menu()
 
-# When I tried to declare this variable inside the calculate_calories function, 
-# it kept throwing an error message as "not defined" when passed into the 
-# update_calories_worksheet function at the
-calories_data = []
 
+def display_recent_weight():
+    """
+    Displays the most recent weight entries in order to keep track of weight
+    gain or weight loss.
+    """
+    
+    weight = SHEET.worksheet("weight")
+    # column = weight.col_values(3)
+    # print(column)
+    # input("do something")
+    
+    columns = []
+    row_length = len(weight.row_values(1)) + 1
+    for ind in range(1, row_length):
+        column = weight.col_values(ind)
+        columns.append(column[-5:])
+    # pprint(columns)
+    residents = weight.row_values(1)
+    for i, j in zip(residents, columns):
+        print(f"{i}'s last five weight readings:")
+        print(j)
+
+    selection = input("Use any key to return to the previous menu.\n")
+    if selection:
+        weight_log_menu()
+    else:
+        pass
+            
 
 def calculate_calories(weight_data):
     """
@@ -83,9 +105,7 @@ def calculate_calories(weight_data):
     project, I followed the general guideline that a cat requires 45 calories
     per kg of bodyweight.
     """
-    print(" Calculating everyone's required calories...\n")
-    
-    # calories_data = []
+    calories_data = []
     for i in weight_data:
         calories = i * 45
         calories_data.append(calories)
@@ -100,7 +120,6 @@ def get_recent_weight():
     """
     weight_worksheet = SHEET.worksheet("weight").get_all_values()
     recent_weight_data = weight_worksheet[-1]
-    print(recent_weight_data)
     recent_weight_floats = [float(i) for i in recent_weight_data]
     
     return recent_weight_floats
@@ -111,13 +130,7 @@ def convert_weight_floats(ideal_weight_data):
     Converts the ideal weight values from the current residents worksheet
     to floats.
     """
-    print("converting values to floats\n")
-    # ideal_weight_floats = []
     ideal_weight_floats = [float(i) for i in ideal_weight_data]
-    # ideal_weight_floats.append(float_weight)
-    
-    print("ideal weight floats converted!\n")
-    print(ideal_weight_floats)
 
     return ideal_weight_floats
 
@@ -139,7 +152,6 @@ def calculate_multipliers(weight, ideal_weight):
             multiplier = 1.2
         calorie_multipliers.append(multiplier)
 
-    print("calorie multipliers calculated")
     return calorie_multipliers
 
 
@@ -147,13 +159,11 @@ def calculate_food_required(calorie_data, calorie_multipliers):
     """
     Calculates the calories required for each resident.
     """
-    print("calculating calories")
     calories_required = []
     for i, j in zip(calorie_data, calorie_multipliers):
         calories = i * j
         calories_required.append(calories)
-    print(calories_required)
-    print("calories calculated!")
+    
     return calories_required
 
 
@@ -162,8 +172,8 @@ def display_food(residents, calories):
     Displays the amount of food in grams each resident should get per meal,
     presuming that 1 gram of food is equal to 1 calorie.
     """
-    # clear()
-    print("This is the amount of food in grams each resident should get today.\n")
+    clear()
+    print("This is the amount of food each resident should get today.\n")
     for i, j in zip(residents, calories):
         print(f"{i}:\n{int(j)} grams -- ({int(math.floor(j / 2))} grams per meal).\n")
         
@@ -176,9 +186,30 @@ def display_food(residents, calories):
 
 
 def weight_log_menu():
-    weight_data = get_weight(residents_list)
-    update_weight_worksheet(weight_data)
+    """
+    Displays the options for the user to select.
+    """
+    while True:
+        clear()
+        print(" * * Weight Log Menu * *\n")
+        print("Select from the options below.\n")
+        print("1. Daily Weight Log\n")
+        print("2. Recent Weight Data\n")
+        print("3. Return to Main Menu\n")
 
+        selection = input("Please make your selection:\n")
+
+        if selection == "1":
+            weight_data = get_weight(residents_list)
+            update_weight_worksheet(weight_data)
+            # get_weight(residents_list)
+        elif selection == "2":
+            display_recent_weight(residents_list)
+        elif selection == "3":
+            main()
+        else:
+            input("Please select from the options above:\n")
+        
 
 def food_calculator():
     """
@@ -210,7 +241,7 @@ def display_current_residents():
     residents_data_chart = pd.DataFrame(current_residents, index=[residents_list])
     print(residents_data_chart)
 
-    selection = input(" Use any key to return to the previous menu.\n")
+    selection = input("Use any key to return to the previous menu.\n")
 
     if selection:
         directory_menu()
@@ -244,14 +275,36 @@ def display_past_residents():
         pass
 
 
+def update_worksheets_new_resident(details, weight):
+    """
+    Takes the data input by user in add_new_resident function and updates the
+    current residents and weight worksheets.
+    """
+    print("Updating directory...\n")
+    current_residents_worksheet = SHEET.worksheet("current residents")
+    current_residents_worksheet.append_row(details)
+    weight_worksheet = SHEET.worksheet("weight")
+    res_list_length = len(weight_worksheet.row_values(1))
+    weight_worksheet.update_cell(1, (res_list_length + 1), details[0])
+    col_length = len(weight_worksheet.col_values(1))
+    weight_worksheet.update_cell(col_length, (res_list_length + 1), weight)
+    print("Directory updated!\n")
+    selection = input("Use any key to return to the previous menu.\n")
+
+    if selection:
+        directory_menu()
+    else:
+        pass
+
+
 def add_new_resident():
     """
     Takes input data to create a new resident listing and appends to current
     residents worksheet and weight worksheet.
     """
     clear()
-    print(" * * Add Resident * * ")
-    print(" Enter the new resident's details below.")
+    print(" * * Add Resident * *")
+    print("Enter the new resident's details below.")
     new_resident_details = []
     while True:
 
@@ -353,34 +406,13 @@ def add_new_resident():
     selection = input("Alternatively, please use 'x' to return to the previous menu.\n")
 
     if selection == "y":
-        update_worksheets_new_resident(new_resident_details)
-        new_resident_weight_update(current_weight)
+        update_worksheets_new_resident(new_resident_details, current_weight)
     elif selection == "n":
         add_new_resident()
     elif selection == "x":
         directory_menu()
     else:
-        input(" Please select from the options above.\n")
-
-
-def update_worksheets_new_resident(details):
-    """
-    Takes the data input by user in add_new_resident function and updates the
-    current residents and weight worksheets.
-    """
-    print("Updating directory...\n")
-    current_residents_worksheet = SHEET.worksheet("current residents")
-    current_residents_worksheet.append_row(details)
-    weight_worksheet = SHEET.worksheet("weight")
-    res_list_length = len(weight_worksheet.row_values(1))
-    weight_worksheet.update_cell(1, (res_list_length + 1), details[0])
-    print("Directory updated!\n")
-    selection = input("Use any key to return to the previous menu.\n")
-
-    if selection:
-        directory_menu()
-    else:
-        pass
+        input("Please select from the options above.\n")
 
 
 def remove_resident_selection(list_of_names):
@@ -406,8 +438,8 @@ def remove_resident_selection(list_of_names):
                 continue
                 # input("Please choose from the list of current residents.\n").strip().capitalize()
             elif confirm == "y":
-                reason = input("What is the reason for check out?\n").strip().capitalize()
-                print("Adopted, Deceased, Other?\n")
+                print("What is the reason for check out?\n")
+                reason = input("Adopted, Deceased, Other?\n").strip().capitalize()
                 if reason not in ["Adopted", "Deceased", "Other"]:
                     input("Please provide a valid reason for check out.\n").strip().capitalize()
                     # continue
@@ -446,9 +478,12 @@ def update_worksheets_remove_resident(resident, status):
     res_data_remove.append(status)
     past_worksheet.append_row(res_data_remove)
     print("Directory updated!\n")
-    selection = input("Use any key to return to the main menu.\n")
-    if selection:
-        main()
+    while True:
+        selection = input("Use any key to return to the main menu.\n")
+        if selection:
+            main()
+        else:
+            pass
 
 
 def directory_menu():
@@ -457,15 +492,15 @@ def directory_menu():
     """
     while True:
         clear()
-        print(" * * Resident Directory Menu * *\n")
-        print(" Please select from the options below.\n")
-        print(" 1. Current Residents Directory\n")
-        print(" 2. Past Residents Directory\n")
-        print(" 3. Add New Resident\n")
-        print(" 4. Remove Resident\n")
-        print(" 5. Return to Main Menu\n")
+        print("* * Resident Directory Menu * *\n")
+        print("Please select from the options below.\n")
+        print("1. Current Residents Directory\n")
+        print("2. Past Residents Directory\n")
+        print("3. Add New Resident\n")
+        print("4. Remove Resident\n")
+        print("5. Return to Main Menu\n")
 
-        selection = input(" Please make your selection:\n")
+        selection = input("Please make your selection:\n")
 
         if selection == "1":
             display_current_residents()
@@ -478,7 +513,7 @@ def directory_menu():
         elif selection == "5":
             main()
         else:
-            input(" Please select from the options above:\n")
+            input("Please select from the options above:\n")
 
 
 def main():
@@ -488,7 +523,7 @@ def main():
     """
     while True:
         clear()
-        print(" Feline Pine Cat Retirement Home")
+        print(" * * Feline Pine Cat Retirement Home * *")
         print("""\
 
         |\      _,,,---,,_
@@ -497,14 +532,14 @@ def main():
         '---''(_/--'  `-'\_)   
                         """)
     
-        print(" Welcome to the Feline Pine management system.\n")
-        print(" Please select from the options below.\n")
-        print(" 1. Resident Directory Management\n")
-        print(" 2. Weight Log Menu\n")
-        print(" 3. Food Calculator\n")
-        print(" 4. Exit Management System\n")
+        print("Welcome to the Feline Pine management system.\n")
+        print("Please select from the options below.\n")
+        print("1. Resident Directory Management\n")
+        print("2. Weight Log Menu\n")
+        print("3. Food Calculator\n")
+        print("4. Exit Management System\n")
         
-        selection = input(" Please make your selection:\n")
+        selection = input("Please make your selection:\n")
 
         if selection == "1":
             directory_menu()
@@ -513,15 +548,17 @@ def main():
         elif selection == "3":
             food_calculator()
         elif selection == "4":
-            print(" Thank you for using the Feline Pine Management System.\n")
-            print(" Have a great day!")
+            print("Thank you for using the Feline Pine Management System.\n")
+            print("Have a great day!")
             break
         else:
-            input(" Please select from the options above:\n")
+            input("Please select from the options above:\n")
 
 
-# main()
-remove_resident_selection(residents_list)
+main()
+# remove_resident_selection(residents_list)
+# weight_log_menu()
+# display_recent_weight()
 
 # get_recent_weight()
 
